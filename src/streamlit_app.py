78f4200e54fde23f5ff3899cd685abb6fbb3d8a2
jade-f-t <75073206+jade-f-t@users.noreply.github.com>
2024-01-19 03:43:17 +0800

    feat : add streamlit

---
+++ src/streamlit_app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+def check_password():
+    """Simple password check."""
+    password = st.sidebar.text_input("Enter a password:", type="password")
+    if password == "your_password":
+        return True
+    else:
+        return False
+
+if check_password():
+    st.title("My Secure App")
+    st.write("Here's our first attempt at using data to create a table:")
+    st.write(pd.DataFrame({
+        'first column': [1, 2, 3, 4],
+        'second column': [10, 20, 30, 40]
+    }))
+else:
+    st.sidebar.error("The password you entered is incorrect.")
+
