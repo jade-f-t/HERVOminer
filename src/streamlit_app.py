5ebf1b0ca5760ba9ae6bacf110f4c0ad84ee07b4
jade-f-t <75073206+jade-f-t@users.noreply.github.com>
2024-01-19 04:11:42 +0800

    fix : streamlit

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,22 +1,44 @@
 import streamlit as st
-import pandas as pd 
+import io
+import tempfile
+from func1_blastORF import func1_blastORF
 
 def check_password():
-    """Simple password check."""
-    password = st.sidebar.text_input("Enter a password:", type="password")
-    if password == "SnowMan0122":
-        return True
-    else:
-        return False
+	password = st.sidebar.text_input("Enter a password:", type="password")
+	if password == "SnowMan0122":
+		return True
+	else:
+		return False
+
+def blastp():
+	uploaded_file = st.file_uploader("Upload file", type=['fasta','fa'])
+	output_path = tempfile.mkdtemp() 
+
+	if uploaded_file is not None:
+	    input_file_path = os.path.join(output_path, uploaded_file.name)
+	    with open(input_file_path, "wb") as f:
+	        f.write(uploaded_file.getbuffer())
+	    
+	    error = func1_blastORF(input_file_path, output_path)
+	    if error:
+	        st.error(f"BLASTp error: {error}")
+	    else:
+	        st.success("BLASTp analysis completed.")
+
+		if st.button("Download BLASTp Result"):
+		    output_file = os.path.join(output_path, "blastp_output.txt")
+		    with open(output_file, "rb") as f:
+		        data = f.read()
+		    	st.download_button(label="Download BLASTp Output", data=data, file_name="blastp_output.txt", mime="text/plain")
+			
+
 
 if check_password():
-    st.title("My Secure App")
-    st.write("Here's our first attempt at using data to create a table:")
-    st.write(pd.DataFrame({
-        'first column': [1, 2, 3, 4],
-        'second column': [10, 20, 30, 40]
-    }))
-else:
-    st.sidebar.error("The password you entered is incorrect.")
+	st.title("HERVOminer")
+	st.write("test")
+	blastp()
+	
 
+else:
+	st.sidebar.error("The password you entered is incorrect.")
 
