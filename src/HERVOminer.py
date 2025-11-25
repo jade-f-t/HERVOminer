9c6b9b1e9f13338162edd399c0e59ec4cf5c8329
jade-f-t <ftw0710@gmail.com>
2025-11-26 01:39:17 +0800

    feat : featureCounts allow multimapped, step 1 blastp allow individual database base on vcf

--- src/HERVOminer.py
+++ src/HERVOminer.py
@@ -15,13 +15,13 @@ if __name__ == "__main__":
 	args = parser.parse_args()
 
 	if args.subcmd == 'blastORF':
-		returnValue = func1_blastORF(args.inputPeptide, args.outputPath)
+		returnValue = func1_blastORF(args.inputPeptide, args.outputPath, args.inputVCF)
 
 	elif args.subcmd == 'summarizeAnnotate':
 		returnValue = func2_summarize_annotation(args.blastpOutput, args.outputPath)
 
 	elif args.subcmd == 'quantification':
-		returnValue = func3_quantification(args.inputCsvFile, args.annotationFile, args.outputPath,args.threadNo, args.parallelTask)
+		returnValue = func3_quantification(args.inputCsvFile, args.annotationFile, args.outputPath,args.threadNo, args.parallelTask, args.multiMapped)
 
 	elif args.subcmd == 'outputResult':
 		returnValue = func4_outputResult(args.inputPeptide, args.inputCsvFile, args.tumourResultDirectories, args.normalResultDirectories, args.outputPath, args.withZero, args.dpi, args.selectedPeptide, args.selectedRegion)
@@ -31,10 +31,11 @@ if __name__ == "__main__":
 		annotationFile = f"{args.outputPath}/quantification.gtf"
 		tumourResultDirectories = f"{args.outputPath}/resultDirectories_T.csv"
 		normalResultDirectories = f"{args.outputPath}/resultDirectories_N.csv"
-		func1_blastORF(args.inputPeptide, args.outputPath)
+		func1_blastORF(args.inputPeptide, args.outputPath, args.inputVCF)
 		func2_summarize_annotation(blastpOutput, args.outputPath)
-		func3_quantification(args.inputCsvFile, annotationFile, args.outputPath,args.threadNo, args.parallelTask)
-		func4_outputResult(args.inputPeptide, args.inputCsvFile, args.tumourResultDirectories, args.normalResultDirectories, args.outputPath, args.withZero, args.dpi, args.selectedPeptide, args.selectedRegion)
+		func3_quantification(args.inputCsvFile, annotationFile, args.outputPath,args.threadNo, args.parallelTask, args.multiMapped)
+		func4_outputResult(args.inputPeptide, args.inputCsvFile, tumourResultDirectories, normalResultDirectories, args.outputPath, args.withZero, args.dpi, None, None)
+
 		returnValue = None
 
 	sys.exit(returnValue)
